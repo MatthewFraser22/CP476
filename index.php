e337192c72979dd9ff69bd64a31d2c6033834c45
Matthew Francis <mattfrancis888@gmail.com>
2023-02-18 14:09:07 -0500

    Add table for homepage.

--- index.php
+++ index.php
@@ -7,9 +7,28 @@
     <div class="header">
         <a href="#default" class="logo">EasyMarking</a>
         <div class="header-right">
-            <a class="active" href="#home">Class List</a>
+            <a class="active" href="#home">Course List</a>
         </div>
     </div>
+
+    <div style="overflow-x:auto;">
+        <table>
+            <tr>
+                <th>Course Code </th>
+                <th>Course Name</th>
+            </tr>
+            <tr>
+                <td>CP476</td>
+                <td>Internet Computing</td>
+            </tr>
+            <tr>
+                <td>CP312</td>
+                <td>Algorithims</td>
+            </tr>
+
+        </table>
+    </div>
+
 </body>
 
 </html>
