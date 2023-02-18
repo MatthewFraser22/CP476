93bd26a1cd19e056e6cde9db9926dbb4317ed4fe
Matthew Francis <mattfrancis888@gmail.com>
2023-02-18 14:01:23 -0500

    set up css

---
+++ index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/index.css" type="text/css">
+
+
+<html>
+
+<body>
+    <div class="header">
+        <a href="#default" class="logo">EasyMarking</a>
+        <div class="header-right">
+            <a class="active" href="#home">Class List</a>
+        </div>
+    </div>
+</body>
+
+</html>
