63cab792603b7414f3158fc2ad2059bbe2db340d
Matthew Francis <mattfrancis888@gmail.com>
2023-02-19 17:04:03 -0500

    Add logo.

--- header.php
+++ header.php
@@ -1,6 +1,11 @@
 <link rel="stylesheet" href="css/header.css" type="text/css">
 <div class="header">
-    <a href="./index.php" class="logo">EasyMarking</a>
+    <div class="logoContainer">
+        <a href="./index.php" class="logo">
+            <img src=" logo.jpg" alt="logo" />
+        </a>
+        <a href="./index.php" class="logo">EasyMarking</a>
+    </div>
     <div class="header-right">
         <a class="active" href="./index.php">Course List</a>
     </div>
