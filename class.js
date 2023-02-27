a92e4431a264d2c2dee256b2eb5290202310ff0a
Matthew Francis <mattfrancis888@gmail.com>
2023-02-27 00:01:00 -0500

    Able to update name.

--- class.js
+++ class.js
@@ -1,3 +1,13 @@
 function redirect(id, course) {
     window.location.href=`marks.php?id=${id}&course=${course}`
   }
+
+  function showDiv(index) {
+    document.getElementsByClassName('editForm')[index].style.visibility = "visible";
+ 
+    document.getElementsByClassName('edit')[index].style.display = "none";
+ }
+
+ function limitDecimalPlaces(event) {
+    this.value = parseFloat(this.value).toFixed(0);
+  }
--- class.php
+++ class.php
@@ -17,22 +17,48 @@
                 <tr>
                     <th>Student ID</th>
                     <th>Full Name</th>
+                    <th>Edit Name</th>
                 </tr>
 
                 <?php
                 $db = new database();
-                $rows = $db->query('SELECT * FROM students WHERE student_id = :id', [":id" => $_GET['id']])->fetchAll();
+                $rows = $db->query('SELECT *  FROM courses INNER JOIN students ON students.student_id = courses.student_id
+                WHERE courses.student_id = :id AND course_code = :course', [":id" => $_GET['id'], ":course" => $_GET['course']])->fetchAll();
 
                 $count = count($rows);
                 // echo $count;
                 // print_r($rows);
                 // echo  $_GET['id'];
+
+                function postHelper($i)
+                {
+                    $data = [
+                        'name' => $_POST["name{$i}"], "id" => $_GET['id']
+                    ];
+                    $db = new database();
+                    $rows = $db->query("UPDATE students SET name=:name WHERE student_id = :id", $data)->fetchAll();
+                    header("Refresh:0");
+                }
+                $x = 1;
                 foreach ($rows as $row) {
+
                     echo "
-                    <tr  onclick='redirect(`{$row['student_id']}`, `{$_GET['course']}`)'>
-                    <td>{$row["student_id"]}</td>
-                    <td>{$row["name"]}</td>
+                    <tr>
+                    <td  onclick='redirect(`{$row['student_id']}`, `{$_GET['course']}`)' >{$row["student_id"]}</td>
+                    <td  onclick='redirect(`{$row['student_id']}`, `{$_GET['course']}`)'>{$row["name"]}</td>
+                                            <td><button class='edit blueButton' type='button' onclick='showDiv($x - 1)'>Edit</button>
+                        <form method='post' class='editForm' style='visibility: hidden; display:inline;'>
+                            <label for='name{$x}'>New Grade: </label>
+    
+                            <input  onkeydown='return /[a-z]/i.test(event.key)'  name='name{$x}'' placeholder='Enter characters' />
+                            <button class='blueButton' type='submit' value='Submit'>Submit </button>
+                        </form>
                     </tr>";
+                    if (isset($_POST["name{$x}"])) {
+
+                        postHelper($x);
+                    }
+                    $x++;
                 }
                 ?>
             </table>
